b760a1ba8734a8c2acc14a85b5642793a1db8549
georgeh02 <75747720+georgeh02@users.noreply.github.com>
2023-09-14 20:56:46 -0700

    adding 3 new features
    button to choose skin, particle effect that matches skin color, jump using space bar

--- Roll-a-Ball/Rollaball/Assets/Scripts/PlayerController.cs
+++ Roll-a-Ball/Rollaball/Assets/Scripts/PlayerController.cs
@@ -7,6 +7,8 @@ using TMPro;
 public class PlayerController : MonoBehaviour
 {
     public float speed = 0;
+    public float jumpAmount = 15;
+    public float gravityScale = 5;
     public TextMeshProUGUI countText;
     public GameObject winTextObject;
 
@@ -15,11 +17,21 @@ public class PlayerController : MonoBehaviour
     private float movementX;
     private float movementY;
 
+    public Material[] material;
+    public int skin;
+    private Renderer rend;
+    public ParticleSystem ps;
+
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
         count = 0;
+
+        skin = 0;
+        rend = GetComponent<Renderer>();
+        rend.enabled = true;
+        rend.sharedMaterial = material[skin];
 
         SetCountText();
         winTextObject.SetActive(false);
@@ -42,11 +54,36 @@ public class PlayerController : MonoBehaviour
         }
     }
 
+    void Update()
+    {
+        rend.sharedMaterial = material[skin];
+
+        if (Input.GetKeyDown(KeyCode.Space) && transform.position.y == 0.5)
+        {
+            rb.AddForce(Vector3.up * jumpAmount, ForceMode.Impulse);
+        }
+    }
+
+    public void ChooseSkin()
+    {
+        if(skin < 2)
+        {
+            skin++;
+        }
+        else
+        {
+            skin = 0;
+        }
+
+        ps.startColor = material[skin].color;
+    }
+
     void FixedUpdate()
     {
         Vector3 movement = new Vector3(movementX, 0.0f, movementY);
 
         rb.AddForce(movement * speed);
+        rb.AddForce(Physics.gravity * (gravityScale - 1) * rb.mass);
     }
 
     private void OnTriggerEnter(Collider other)
