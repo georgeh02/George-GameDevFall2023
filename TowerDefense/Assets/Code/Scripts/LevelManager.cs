cc66f14690bb3b9f36079e70223c432786a4666b
georgeh02 <75747720+georgeh02@users.noreply.github.com>
2023-10-03 16:26:52 -0700

    devlog 5

--- TowerDefense/Assets/Code/Scripts/LevelManager.cs
+++ TowerDefense/Assets/Code/Scripts/LevelManager.cs
@@ -9,9 +9,34 @@ public class LevelManager : MonoBehaviour
     public Transform startPoint;
     public Transform[] path;
 
+    public int currency;
+
     private void Awake()
     {
         main = this;
+    }
+
+    private void Start()
+    {
+        currency = 100;
+    }
+
+    public void IncreaseCurrency(int amount)
+    {
+        currency += amount;
+    }
+
+    public bool SpendCurrency(int amount)
+    {
+        if (amount <= currency)
+        {
+            currency -= amount;
+            return true;
+        } else
+        {
+            Debug.Log("need more guap");
+            return false;
+        }
     }
 
 }
